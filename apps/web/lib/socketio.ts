70d7fb51dcba5e6034ab3802e88372d12722b834
Federico Luna <federicolun4@gmail.com>
2024-12-29 20:53:09 -0300

    Update socket connection to use DOMAIN_NAME environment variable

--- apps/web/lib/socketio.ts
+++ apps/web/lib/socketio.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.DOMAIN_URL || "http://localhost:3001"}`, {
-  // path: `${process.env.DOMAIN_URL ? '/socket.io' : ""}`
+const socket = io(`${process.env.DOMAIN_NAME || "http://localhost:3001"}`, {
+  // path: `${process.env.DOMAIN_NAME ? '/socket.io' : ""}`,
   transports: ['websocket']
 });
 
